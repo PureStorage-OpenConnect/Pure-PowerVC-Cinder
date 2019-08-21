0f55b8fdb80d753866cd705135c093502ed081cc
Simon Dodsley <simon@purestorage.com>
2019-08-21 11:43:30 -0400

    Change year in copyright statement

--- pure_powervc.py
+++ pure_powervc.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2014 Pure Storage, Inc.
+# Copyright (c) 2019 Pure Storage, Inc.
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
